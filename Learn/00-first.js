9a38ed3660b1f1f38d7427f54e70604fc75ff82e
DURRVenture <durrdurrgamer@gmail.com>
2024-12-15 21:19:13 -0600

    variables

---
+++ Learn/00-first.js
@@ -0,0 +1,33 @@
+/*comments
+	//
+	/*
+*/
+
+/*
+variables 
+data types	
+*/
+ls = 2
+
+var a = "s" // strings
+let hi = "js"
+const s = 2 // num
+
+//boolean
+let student = true // false 
+
+let underfine = undefined
+let nu = null 
+
+// symbol
+let m = Symbol("m")
+
+// number 
+let big = BigInt(22222222222222222222222222222222)
+let big0 = 22222222222222222222222222222222n
+
+//prints
+console.log("hi", hi)
+//data type
+console.log(typeof nu) // null is object
+
---
+++ Learn/01-exercises.js
@@ -0,0 +1,44 @@
+//0. Write a single-line comment
+
+/* 1. 
+Write a multi-line comment
+*/
+
+//2. Declare variables with values assigned to all primitive data types 
+var a = "a"
+let s = "S"
+const d = "d"
+let f = Symbol("s")
+let g = null
+let undefine = undefined
+let h = true // false
+let j = BigInt(33333333333333333333333333) // 222222222222222222222222n
+
+//3. Print the value of all variables to the console
+console.log(a)
+
+//4. Print the type of all variables to the console
+console.log(typeof j)
+
+//5. Modify the values of the variables to others of the same type
+a = "A"
+console.log(a)
+
+//6. Modify the values of the variables to others of a different type
+a = 2
+console.log(a)
+
+//7. Declare constants with values assigned to all primitive data types
+const q = "a"
+const w = "S"
+const e = "d"
+const r = Symbol("s")
+const t = null
+const y = true // false
+const u = BigInt(33333333333333333333333333) // 222222222222222222222222n
+
+//8. Attempt to modify the values of the constants
+//q = 4
+
+//9. Comment out lines that produce any type of error when execut
+//TypeError: Assignment to constant variable.
