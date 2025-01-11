315a4af2ed3a778db9c1d225b24fea14ed10bca8
DURRVenture <durrdurrgamer@gmail.com>
2025-01-10 18:01:30 -0600

    operators

--- Learn/01-exercises.js
+++ Learn/01-exercises.js
@@ -1,8 +1,11 @@
+/*
+
+
 //0. Write a single-line comment
 
 /* 1. 
 Write a multi-line comment
-*/
+
 
 //2. Declare variables with values assigned to all primitive data types 
 var a = "a"
@@ -42,3 +45,76 @@ const u = BigInt(33333333333333333333333333) // 222222222222222222222222n
 
 //9. Comment out lines that produce any type of error when execut
 //TypeError: Assignment to constant variable.
+*/
+
+/*
+
+
+// 1. Create a variable for each arithmetic operation
+let a = 5;
+let b = 10;
+
+let sum = a + b;
+let subtraction = a - b;
+
+// 2. Create a variable for each type of assignment operation,
+//    using the variables created for arithmetic operations
+
+// // console.log(a + b);
+// // console.log(a - b);
+// // console.log(a * b);
+// // console.log(a / b);
+// // console.log(a & b);
+// // console.log(a ** b);
+// // console.log(a == b);
+// // console.log("a =", a);
+// // console.log("b =", b);
+
+// 3. Print 5 true comparisons using different comparison operators
+
+// console.log("type true");
+console.log(a < b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a == "5");
+console.log(a === 5);
+
+// 4. Print 5 false comparisons using different comparison operators
+
+// console.log("type false");
+console.log(a > b);
+console.log(a >= b);
+console.log(a == b);
+console.log(a === b);
+console.log(a === "f");
+
+// 5. Use the logical AND operator
+
+console.log(10 > 3 && 40 < 2);
+
+// 6. Use the logical OR operator
+
+console.log(10 > 3 || 40 < 2);
+
+// 7. Combine both logical operators
+
+console.log(10 > 10 && 40 < 2 || 20 > 10);
+
+// 8. Add some negation
+
+console.log(!(10 > 10 && 40 < 2 || 20 > 10));
+
+// 9. Use the ternary operator
+
+let isStudent = true;
+
+isStudent ? console.log("yes") : console.log("no");
+
+// 10. Combine arithmetic, comparison, and logical operators
+
+console.log((10 >= 11 && 15 < 8) / 2);
+
+let q = 10, w = 20, e = 5;
+
+console.log(((q + e) > w || w - e < q) && (q * e <= w));
+*/
